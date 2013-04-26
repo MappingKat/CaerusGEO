050825eef2e94ae3e76cd5f6b2659f858d96b23c
Steve Kashishian <steve@studiosanderson.com>
2013-04-25 20:30:34 -0400

    refining the pointsetter control used on uploaded edit pages.

--- app/assets/javascripts/pointsetter.js
+++ app/assets/javascripts/pointsetter.js
@@ -1,55 +1,33 @@
 L.Control.Pointsetter = L.Control.extend({
   options: {
-    position: 'topleft'
-  },
-
-  initialize: function (options) {
-        this._config = {};
-        this.setConfig(options);
-    },
-
-  setConfig: function (options) {
-      this._config = {
-          'entity_name': options.entity_name
-      };
+    position: 'topleft', //Should match Draw's position.
+    entity_name: null
   },
 
   onAdd: function (map) {
     var className = 'leaflet-control-pointsetter',
         container = L.DomUtil.create('div', className);
 
-    this._map = map;
-
- 
-
     this._commitButton = this._createButton(
-            this._config.entity_name, this._config.entity_name,  className + '-in',  container, this._saveCurrent,  this);
-
+            this.options.entity_name, '',  container, this._saveCurrent,  this);
     return container;
   },
 
-  onRemove: function (map) {
-   
-  },
-
   _saveCurrent: function (e) {
-    $(".leaflet-control-pointsetter a").text('Saving');
+    this._commitButton.innerHTML = 'Saving';
     var control = this;
     $('.leaflet-control-pointsetter').popover('hide');
-    var config = this._config;
     EntryServices.LeaveResult(function() {
       $(".leaflet-control-pointsetter").hide('fast');
       $("#ready").hide();
-      $(".leaflet-control-pointsetter a").text(config.entity_name);
+      control._commitButton.innerHTML = control.options.entity_name;
     });
-    
   },
 
-  _createButton: function (html, title, className, container, fn, context) {
+  _createButton: function (html, className, container, fn, context) {
     var link = L.DomUtil.create('a', className, container);
     link.innerHTML = html;
     link.href = '#';
-    link.title = title;
 
     L.DomEvent
         .on(link, 'click', L.DomEvent.stopPropagation)
